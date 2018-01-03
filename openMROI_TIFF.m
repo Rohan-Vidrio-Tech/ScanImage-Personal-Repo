af79a9adfbd130416c75ec0c671f6e1185926f5f
Rohan-Vidrio-Tech <33072912+Rohan-Vidrio-Tech@users.noreply.github.com>
2018-01-03 10:28:18 -0500

    Update openMROI_TIFF.m

--- openMROI_TIFF.m
+++ openMROI_TIFF.m
@@ -3,3 +3,10 @@
 %       	Aout: MxNxCxFxSxV array, with images of size MxN for C channels, F frames, S slices, and V volumes. Default type is uint16. 
 %       	imgInfo: Structure comprising basic information about the structure of the output array Aout
 [header,Aout,imgInfo] = scanimage.util.opentif('C:\Users\rohan\Downloads\Run1_RF_Large_00001.tif')
+
+size(Aout)
+c = mean(Aout,6);
+c = squeeze(c);
+size(c);
+size(c)
+figurer;imagesc(c(:,:,1))
